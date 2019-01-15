96c87cf15057d8a3e0ab4ce66238a3e135075af9
Paul Rosero <paul_rosero@yahoo.com>
2019-01-15 03:11:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- phone.scan(/^\(?([0-9]{3})\)?[- ]?...$/)
+ phone.scan(/([0-9]{10})|([0-9]{3}\s[0-9]{3}\s[0-9]{4})|(\([0-9]{3}\)[0-9]{7})|(\([0-9]{3}\)[0-9]{3}\-[0-9]{4})/)
 end
